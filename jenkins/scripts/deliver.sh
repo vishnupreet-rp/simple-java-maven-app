5ebd866c041eea77e1cfc9083e0dfdbf6eb52a3f
Vishnu Preet <vishnupreet.rp@gmail.com>
2024-07-16 11:24:24 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -12,14 +12,14 @@ echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
 # NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
-NAME=$(mvn help:evaluate -Dexpression=project.name|grep -v"^\["|sed 's/\x1b\[[0-9;]*m//g')
+NAME=$(mvn help:evaluate -Dexpression=project.name|grep -v "^\[" | sed 's/\x1b\[[0-9;]*m//g')
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 # VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
-VERSION=`mvn help:evaluate -Dexpression=project.name|grep -E"^[0-9]+\.[0-9]+.*"`
+VERSION=`mvn help:evaluate -Dexpression=project.name|grep -E "^[0-9]+\.[0-9]+.*"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
